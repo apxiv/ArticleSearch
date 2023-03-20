7dfded213cdbeefb0f9e66b08d3bbe1356f7ff75
apxiv <89283600+apxiv@users.noreply.github.com>
2023-03-20 18:51:18 -0400

    json setup and intent

---
+++ app/src/main/java/com/codepath/articlesearch/Byline.kt
@@ -0,0 +1,11 @@
+package com.codepath.articlesearch
+import android.support.annotation.Keep
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Keep
+@Serializable
+data class Byline(
+    @SerialName("original")
+    val original: String? = null
+) : java.io.Serializable
---
+++ app/src/main/java/com/codepath/articlesearch/MultiMedia.kt
@@ -0,0 +1,11 @@
+package com.codepath.articlesearch
+import android.support.annotation.Keep
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Keep
+@Serializable
+data class MultiMedia(
+    @SerialName("url")
+    val url: String?
+) : java.io.Serializable
